69ec9612d0c8459027927342ba242c9793040548
Andrew Fleming <ajf9@hw.ac.uk>
2012-11-10 20:47:56 +0000

    Cloned from Yohann's Branch

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
+<?php 
 
+/**
+ * Autoloading classes from model folder when PHP instruction 'new MyClass'
+ * @param String $className
+ */
+function loadClass($className)
+{
+	require_once 'model/'.$className.'.class.php';
+}
+spl_autoload_register ('loadClass');
+
+/**
+ * DATABASE CONNECTION, with PDO object
+ * All variables are defined in config/config.php
+ */
+require_once 'config/config.php';
+
+try 
+{
+	$db = new PDO("mysql:host=$server;dbname=$database",$user,$password);
+} 
+catch (Exception $e) 
+{
+	echo "Connection error".$e->getMessage();
+}
+
+require_once 'controller/controller.php';
+
+
+?>
