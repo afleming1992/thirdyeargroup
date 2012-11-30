4880635189a0a96bead2e2165a49482b0a0b091b
Yohann HAUTION <yohann.haution@gmail.com>
2012-11-30 20:03:57 +0000

    remove the URL rewriting

--- view/navbar.php
+++ view/navbar.php
@@ -3,13 +3,13 @@
     <div class="container">
       <ul class="nav">
         <li> <a href="index.php">Home</a> </li>
-        <li> <a href="about-us.html">About us</a> </li>
-        <li> <a href="wattball.html">WattBall</a> </li>
-        <li> <a href="men-hurdles.html">Men's Hurdles</a> </li>
-        <li> <a href="female-hurdles.html">Female's Hurdles</a> </li>
+        <li> <a href="index.php?page=aboutUs">About us</a> </li>
+        <li> <a href="index.php?page=wattBall">WattBall</a> </li>
+        <li> <a href="index.php?page=menHurdles">Men's Hurdles</a> </li>
+        <li> <a href="index.php?page=femaleHurdles">Female's Hurdles</a> </li>
         <?php 
         if(isset($_SESSION['login']) && $_SESSION['login'] == true)
-        	echo "<li> <a href='staff.html'>Staff</a> </li>";
+        	echo "<li> <a href='index.php?page=homeStaff'>Staff</a> </li>";
         ?>
       </ul>
     </div>
