e697312effbc0432d9811df8e3e3c2df9d8e6338
Andrew Fleming <ajf9@hw.ac.uk>
2012-12-19 14:20:15 +0000

    Added Constructor, getinfo, updateinfo and addteam functions to WattBall Team Model

--- model/team.class.php
+++ model/team.class.php
@@ -2,97 +2,155 @@
 
 class Team
 {
-	private $teamID;
-	private $teamName;
-	private $tournamentID;
-	private $contactName;
-	private $contactNumber;
-	private $nwaNumber;
-	private $email;
+		private $teamID;
+		private $teamName;
+		private $tournamentID;
+		private $contactName;
+		private $contactNumber;
+		private $nwaNumber;
+		private $email;
+		private $db;
+
+
+	/*--- Constructor ---*/
+	public function __construct($db)
+	{
+		$this->setDb($db);
+	}
+
+	/*--- Method Calls ---*/
+
+	// Add New Team
 	
-	private $db;
-}
+	public function addTeamInfo()
+	{
+		$result = $this->db->query("INSERT INTO wattball_team VALUES ('0','".$this->tournamentID."','".$this->teamName."','".$this->contactName."','".$this->contactNumber."','".$this->nwaNumber."','".$this->email."'");
+		if($result != false)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	// Update Team
+	public function updateTeamInfo()
+	{
+		$result = $this->db->query("UPDATE wattball_team SET teamID='".$this->teamID."', tournamentID = '".$this->tournamentID."', contactName = '".$this->contactName"', contactNumber = '".$this->contactNumber."', NWANumber = '".$this->nwaNumber."', email = '".$this->email."' WHERE teamID = '"$this->teamID"'");
+		if($result != false)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	// Get Team Details
+	public function getTeamInfo()
+	{
+		$result =$this->db->query("SELECT * FROM wattball_team SET teamName='".$this->teamName."'");
+		if ($result!=false)
+		{
+			while($data = $result->fetch())
+			{
+				$this->setTeamName($data['teamName']);
+				$this->setTournamentID($data['tournamentID']);
+				$this->setContactName($data['contactName']);
+				$this->setContactNumber($data['contactNumber']);
+				$this->setNwaNumber($data['NWANumber']);
+				$this->setEmail($data['email']);
+			}
+			return true;
+		}
+		else
+			return false;
+	}
 
-/* ----- GETTERS AND SETTERS ----- */
+	/* ----- GETTERS AND SETTERS ----- */
 
-public function getTeamId()
-{
-	    return $this->teamID;
-}
+	public function getTeamId()
+	{
+			return $this->teamID;
+	}
 
-public function setTeamID($teamID)
-{
-	$this->teamID = $teamID;
-}
+	public function setTeamID($teamID)
+	{
+		$this->teamID = $teamID;
+	}
 
-public function getTeamName()
-{
-	    return $this->teamName;
-}
+	public function getTeamName()
+	{
+			return $this->teamName;
+	}
 
-public function setTeamName($teamName)
-{
-	$this->teamName = $teamName;
-}
+	public function setTeamName($teamName)
+	{
+		$this->teamName = $teamName;
+	}
 
-public function getTournamentID()
-{
-	    return $this->tournamentID;
-}
+	public function getTournamentID()
+	{
+			return $this->tournamentID;
+	}
 
-public function setTournamentID($tournamentID)
-{
-	$this->tournamentID = $tournamentID;
-}
+	public function setTournamentID($tournamentID)
+	{
+		$this->tournamentID = $tournamentID;
+	}
 
-public function getContactName()
-{
-	    return $this->contactName;
-}
+	public function getContactName()
+	{
+			return $this->contactName;
+	}
 
-public function setContactName($contactName)
-{
-	$this->contactName = $contactName;
-}
+	public function setContactName($contactName)
+	{
+		$this->contactName = $contactName;
+	}
 
-public function getContactNumber()
-{
-	    return $this->contactNumber;
-}
+	public function getContactNumber()
+	{
+			return $this->contactNumber;
+	}
 
-public function setContactName($contactNumber)
-{
-	$this->contactNumber = $contactNumber;
-}
+	public function setContactName($contactNumber)
+	{
+		$this->contactNumber = $contactNumber;
+	}
 
-public function getNwaNumber()
-{
-	    return $this->nwaNumber;
-}
+	public function getNwaNumber()
+	{
+			return $this->nwaNumber;
+	}
 
-public function setNwaNumber($nwaNumber)
-{
-	$this->nwaNumber = $nwaNumber;
-}
+	public function setNwaNumber($nwaNumber)
+	{
+		$this->nwaNumber = $nwaNumber;
+	}
 
-public function getEmail()
-{
-	    return $this->email;
-}
+	public function getEmail()
+	{
+			return $this->email;
+	}
 
-public function setEmail($email)
-{
-	$this->email = $email;
-}
+	public function setEmail($email)
+	{
+		$this->email = $email;
+	}
 
-public function getDb()
-{
-	   return $this->db;
-}
+	public function getDb()
+	{
+		   return $this->db;
+	}
 
-public function setDb($db)
-{
-	    $this->db = $db;
+	public function setDb($db)
+	{
+			$this->db = $db;
+	}
+
 }
 	
 ?>
