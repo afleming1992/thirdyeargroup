679670a12637ce1d2c07b0fbb4b95e50a68e9299
Andrew Fleming <ajf9@hw.ac.uk>
2013-01-30 16:12:27 +0000

    Whoops! Forgot to add the Untracked Files!

---
+++ config/Websiteconfig.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ *  DATABASE CONFIGURATION
+ */
+$server = "mysql15.000webhost.com";
+$database = "a7743805_sports";
+$password = "techdev2013";
+$user = "a7743805_tech";
+
+?>
---
+++ view/hurdleNav.php
@@ -0,0 +1,14 @@
+<div class='row-fluid'>
+    <div class="span3 contentbox">
+        <ul class="nav nav-list">
+           <li <?php if($pageName == 'wattBall'){ echo "class='active'";}  ?>>
+                <a href="?page=wattBall">Results for Tournament</a>
+          </li>
+          <li <?php if($pageName == 'wattBallScheduling'){ echo "class='active'";}  ?>>
+            <a href="?page=wattBallScheduling">Schedule for Tournament</a>
+          </li>
+          <li <?php if($pageName == 'wattBallRegistration'){ echo "class='active'";}  ?>>
+                  <a href="?page=wattBallRegistration">Register for Tournament</a>
+          </li>
+        </ul>
+    </div>
