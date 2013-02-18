5f7b7f644544ba860545a6f3e2c90e467a32d6b6
Blair Russell <br53@hw.ac.uk>
2013-02-18 21:41:43 +0000

    Added form validation for Umpire Management

--- adminView/umpireManagement.php
+++ adminView/umpireManagement.php
@@ -1,95 +1,97 @@
 <div class='span9 contentbox'>
-<?php
 
-if ($staff->getManager()== 1)
-{
-	?>
 	<script src="javascript/umpireManagement.js"> </script>    
 	<div id="umpireList">
-		<fieldset>
-		<legend>All Umpires</legend>
-			<table  class='table table-hover table-bordered'>
-		  		<tr>
-					<th>Name</th>
-					<th>Email</th>
-					<th>Change</th>
-					<th>Delete</th>
-		  		 </tr>	
-		  		<?php 
-		  			$allUmpires = $this->umpire;		  			
-		  			for($i=0;$i<sizeof($allUmpires);$i++)
-		  			{
-		  				echo "<td class='umpireName'>".$allUmpires[$i]->getName()."</td>";
-		  				echo "<td class='umpireEmail'>".$allUmpires[$i]->getEmail()."</td>";
-		  				echo "<td class='slot0' hidden='true'>".$allUmpires[$i]->getMonMorning()."</td>";
-						echo "<td class='slot1' hidden='true'>".$allUmpires[$i]->getMonAfternoon()."</td>";
-						echo "<td class='slot2' hidden='true'>".$allUmpires[$i]->getTuesMorning()."</td>";
-						echo "<td class='slot3' hidden='true'>".$allUmpires[$i]->getTuesAfternoon()."</td>";
-						echo "<td class='slot4' hidden='true'>".$allUmpires[$i]->getWedMorning()."</td>";
-						echo "<td class='slot5' hidden='true'>".$allUmpires[$i]->getWedAfternoon()."</td>";
-						echo "<td class='slot6' hidden='true'>".$allUmpires[$i]->getThursMorning()."</td>";
-						echo "<td class='slot7' hidden='true'>".$allUmpires[$i]->getThursAfternoon()."</td>";
-						echo "<td class='slot8' hidden='true'>".$allUmpires[$i]->getFriMorning()."</td>";
-						echo "<td class='slot9' hidden='true'>".$allUmpires[$i]->getFriAfternoon()."</td>";
-						echo "<td class='slot10' hidden='true'>".$allUmpires[$i]->getSatMorning()."</td>";
-						echo "<td class='slot11' hidden='true'>".$allUmpires[$i]->getSatAfternoon()."</td>";
-						echo "<td class='slot12' hidden='true'>".$allUmpires[$i]->getSunMorning()."</td>";
-						echo "<td class='slot13' hidden='true'>".$allUmpires[$i]->getSunAfternoon()."</td>";						
-		  				echo "<td><button type='button' data-toggle='modal' data-target='#editUmpire' id='".$allUmpires[$i]->getID()."' class='btn btn-warning btn-mini'><i class='icon-white  icon-wrench'</i></button></td>";
-						echo "<td><button id='".$allUmpires[$i]->getID()."' class='btn btn-danger btn-mini'><i class='icon-white icon-remove-sign'</i></button></td>";		  				
-		  				echo "</tr>";
-		  			}
-		  		?>
-			</table>
-			<?php 
-				if(sizeof($allUmpires)==0)
-					echo "There are no umpires."
-			?>	
-		</fieldset>
+		<?php 
+			$allUmpires = $this->umpire;
+			include("include/allUmpires.php"); 
+		?>	
 	</div>
 
 	<!-- Div add umpire  -->
 		
-		<div id="addUmpire" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
-			<div class="modal-header">
-	    		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-	   			 <h3 id="myModalLabel">Add an umpire</h3>
-	 		</div>
-	 		<div class="modal-body">
-	                    <div id="addUmpireName">
-				<label for="umpireName">Name: </label>
-				<input type="text" name="umpireName" id="umpireName"></br><span id="help-inline-umpireName" class="help-inline"></span></br>
-	                    </div>
-	                    <div id="addUmpireEmail">
-				<label for="umpireEmail">Email: </label>
-				<input type="text" id="umpireEmail" name="umpireEmail"></br><span id="help-inline-umpireEmail" class="help-inline"></span>
-	                    </div>
-	                    <h5>Availability</h5> 
-	                    <label class="checkbox">Monday Morning <input type="checkbox" id="slot0" ></label>
-	                    <label class="checkbox">Monday Afternoon <input type="checkbox" id="slot1" ></label> 
-	                    <label class="checkbox">Tuesday Morning <input type="checkbox" id="slot2" ></label> 
-	                    <label class="checkbox">Tuesday Afternoon <input type="checkbox" id="slot3" ></label> 
-	                    <label class="checkbox">Wednesday Morning <input type="checkbox" id="slot4"></label> 
-	                    <label class="checkbox">Wednesday Afternoon <input type="checkbox" id="slot5"></label> 
-	                    <label class="checkbox">Thursday Morning <input type="checkbox" id="slot6" > </label>
-	                    <label class="checkbox">Thursday Afternoon <input type="checkbox" id="slot7" ></label> 
-	                    <label class="checkbox">Friday Morning <input type="checkbox" id="slot8" ></label> 
-	                    <label class="checkbox">Friday Afternoon <input type="checkbox" id="slot9" ></label> 
-	                    <label class="checkbox">Saturday Morning <input type="checkbox" id="slot10" ></label> 
-	                    <label class="checkbox">Saturday Afternoon <input type="checkbox" id="slot11" ></label> 
-	                    <label class="checkbox">Sunday Morning <input type="checkbox" id="slot12" > </label>
-	                    <label class="checkbox">Sunday Afternoon <input type="checkbox" id="slot13" ></label>                    
+	<div id="addUmpire" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
+		<div class="modal-header">
+    		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+   			 <h3 id="myModalLabel">Add an umpire</h3>
+ 		</div>
+ 		<div class="modal-body">
+                    <div id="addUmpireName">
+			<label for="umpireName">Name: </label>
+			<input type="text" name="umpireName" id="umpireName"></br><span id="help-inline-umpireName" class="help-inline"></span></br>
+                    </div>
+                    <div id="addUmpireEmail">
+			<label for="umpireEmail">Email: </label>
+			<input type="text" id="umpireEmail" name="umpireEmail"></br><span id="help-inline-umpireEmail" class="help-inline"></span>
 			</div>
-			 <div class="modal-footer">
-			    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-			    <button type='button' value='submit' aria-hidden="true" id='addUmpireValidation' class="btn btn-primary btn"><i class="icon-white icon-ok-sign"></i> Save</button>
-	  		</div>
+			<fieldset>
+				<h5><u>Availability</u></h5>
+				
+				<table class="table table-hover">
+					<thead>
+					<tr>
+						<td></td>
+						<th>Morning</th>
+						<th>Afternoon</th>
+					</tr>
+					</thead>
+					<tbody>
+					<tr>
+						<td>Monday</td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slot0" /></label></td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slot1" /></label></td> 
+                    </tr>
+					<tr>
+						<td>Tuesday</td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slot2" /></label></td> 
+	                    <td><label class="checkbox"><input type="checkbox" id="slot3" /></label></td> 
+                    </tr>
+					<tr>
+						<td>Wednesday</td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slot4" /></label></td> 
+	                    <td><label class="checkbox"><input type="checkbox" id="slot5" /></label></td> 
+                    </tr>
+					<tr>
+						<td>Thursday</td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slot6" /></label></td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slot7" /></label></td> 
+                    </tr>
+					<tr>
+						<td>Friday</td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slot8" /></label></td> 
+	                    <td><label class="checkbox"><input type="checkbox" id="slot9" /></label></td> 
+                    </tr>
+					<tr>
+						<td>Saturday</td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slot10" /></label></td> 
+	                    <td><label class="checkbox"><input type="checkbox" id="slot11" /></label></td>
+                    </tr>
+					<tr> 
+						<td>Sunday</td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slot12" /></label></td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slot13" /></label></td> 
+                    </tr>
+                    </tbody>
+                    <tfoot>
+						<tr>
+							<td></td>
+							<td></td>
+							<td><input type="checkbox" class="checkall"> Check all</input></td>
+						</tr>
+                    </tfoot>
+				</table>
+			</fieldset>                  
 		</div>
-		
-		<button type="button" data-toggle="modal" class="btn btn-primary btn-medium" data-target="#addUmpire"><i class="icon-plus-sign icon-white"></i> Add Umpire</button>
+		 <div class="modal-footer">
+		    <button class="btn" id='addClose' data-dismiss="modal" aria-hidden="true">Close</button>
+		    <button type='button' value='submit' aria-hidden="true" id='addUmpireValidation' class="btn btn-primary btn"><i class="icon-white icon-ok-sign"></i> Save</button>
+  		</div>
+	</div>
+	
+	<button type="button" data-toggle="modal" class="btn btn-primary btn-medium" data-target="#addUmpire"><i class="icon-plus-sign icon-white"></i> Add Umpire</button>
+
 
 	<!-- Div edit umpire  -->
-	
 	
 	<div id="editUmpire" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
 		<div class="modal-header">
@@ -97,50 +99,92 @@ if ($staff->getManager()== 1)
    			 <h3 id="myModalLabel">Edit an umpire</h3>
  		</div>
  		<div class="modal-body">
+			<div id="editUmpireName">
 			<label for="umpireNameChange">Name: </label>
-			<input type="text" id="umpireNameChange"></br><span id="help-inline-umpireName" class="help-inline"></span></br>
+			<input type="text" id="umpireNameChange" name="umpireNameChange"></br><span id="help-inline-umpireNameChange" class="help-inline"></span></br>
+			</div>
 			
+			<div id="editUmpireEmail">
 			<label for="umpireEmailChange">Email: </label>
-			<input type="text" id="umpireEmailChange" ></br><span id="help-inline-umpireName" class="help-inline"></span></br>
+			<input type="text" id="umpireEmailChange" name ="umpireEmailChange"></br><span id="help-inline-umpireEmailChange" class="help-inline"></span></br>
+			</div>
 			
-			<h5>Availability</h5> 
-	                    <label class="checkbox">Monday Morning <input type="checkbox" id="slott0" ></label>
-	                    <label class="checkbox">Monday Afternoon <input type="checkbox" id="slott1" ></label> 
-	                    <label class="checkbox">Tuesday Morning <input type="checkbox" id="slott2" ></label> 
-	                    <label class="checkbox">Tuesday Afternoon <input type="checkbox" id="slott3" ></label> 
-	                    <label class="checkbox">Wednesday Morning <input type="checkbox" id="slott4"></label> 
-	                    <label class="checkbox">Wednesday Afternoon <input type="checkbox" id="slott5"></label> 
-	                    <label class="checkbox">Thursday Morning <input type="checkbox" id="slott6" > </label>
-	                    <label class="checkbox">Thursday Afternoon <input type="checkbox" id="slott7" ></label> 
-	                    <label class="checkbox">Friday Morning <input type="checkbox" id="slott8" ></label> 
-	                    <label class="checkbox">Friday Afternoon <input type="checkbox" id="slott9" ></label> 
-	                    <label class="checkbox">Saturday Morning <input type="checkbox" id="slott10" ></label> 
-	                    <label class="checkbox">Saturday Afternoon <input type="checkbox" id="slott11" ></label> 
-	                    <label class="checkbox">Sunday Morning <input type="checkbox" id="slott12" > </label>
-	                    <label class="checkbox">Sunday Afternoon <input type="checkbox" id="slott13" ></label>              	
+			<fieldset>
+				<h5><u>Availability</u></h5>
+				
+				<table class="table table-hover">
+					<thead>
+					<tr>
+						<td></td>
+						<th>Morning</th>
+						<th>Afternoon</th>
+					</tr>
+					</thead>
+					<tbody>
+					<tr>
+						<td>Monday</td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slott0" /></label></td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slott1" /></label></td> 
+                    </tr>
+					<tr>
+						<td>Tuesday</td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slott2" /></label></td> 
+	                    <td><label class="checkbox"><input type="checkbox" id="slott3" /></label></td> 
+                    </tr>
+					<tr>
+						<td>Wednesday</td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slott4" /></label></td> 
+	                    <td><label class="checkbox"><input type="checkbox" id="slott5" /></label></td> 
+                    </tr>
+					<tr>
+						<td>Thursday</td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slott6" /></label></td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slott7" /></label></td> 
+                    </tr>
+					<tr>
+						<td>Friday</td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slott8" /></label></td> 
+	                    <td><label class="checkbox"><input type="checkbox" id="slott9" /></label></td> 
+                    </tr>
+					<tr>
+						<td>Saturday</td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slott10" /></label></td> 
+	                    <td><label class="checkbox"><input type="checkbox" id="slott11" /></label></td>
+                    </tr>
+					<tr> 
+						<td>Sunday</td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slott12" /></label></td>
+	                    <td><label class="checkbox"><input type="checkbox" id="slott13" /></label></td> 
+                    </tr>
+                    </tbody>
+                    <tfoot>
+						<tr>
+							<td></td>
+							<td></td>
+							<td><input type="checkbox" class="checkall"> Check all</input></td>
+						</tr>
+                    </tfoot>
+				</table>
+			</fieldset>                    	
 		</div>
-		 <div class="modal-footer">
-		    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+		<div class="modal-footer">
+		    <button class="btn" id='editClose' data-dismiss="modal" aria-hidden="true">Close</button>
 		    <button type='button' value='submit' id='editUmpireValidation' class="btn btn-primary btn-medium"><i class="icon-white icon-ok-sign"></i> Save</button>
   		</div>
 	</div>
         
-        <!-- Div delete umpire  -->
-        
-        <div id="deleteUmpire" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
-            <div class="modal-header">
-    		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-   			 <h3 id="myModalLabel">Are you sure you want to delete this Umpire ?</h3>
-            </div>
-            <div class="modal-body center">
-                <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-		<button type='button' id='buttonDeleteUmpire' class="btn btn-danger btn-medium"><i class="icon-white icon-remove-sign"></i> Delete</button>
-            </div>
-        </div>
+	<!-- Div delete umpire  -->
+	
+	<div id="deleteUmpire" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
+		<div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+		 <h3 id="myModalLabel">Are you sure you want to delete this Umpire?</h3>
+		</div>
+		<div class="modal-body center">
+			<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+	<button type='button' id='buttonDeleteUmpire' class="btn btn-danger btn-medium"><i class="icon-white icon-remove-sign"></i> Delete</button>
+		</div>
+	</div>
 	
 </div>
 </div>
-	<?php 
-}
-
-?>
--- javascript/umpireManagement.js
+++ javascript/umpireManagement.js
@@ -1,23 +1,38 @@
 $(document).ready(function() 
 {
       //Form control
-        var validForm = false;
-        var validName = false;
-        var validEmail = false;
-        function formControl()
+        var validForm;
+        var validName;
+        var validEmail;
+        
+        function formControl(option)
         {
-            var Name = $('#umpireName').val();
-            var Email = $('#umpireEmail').val();
+			var Name;
+			var Email;
+			var regexEmail;
+			validForm=false;
+			validName=false;
+			validEmail=false;
+			
+			if(option === "add")
+			{
+				Name = $('#umpireName').val();
+				Email = $('#umpireEmail').val();
+			}
+			else if(option === "edit")
+			{
+				Name = $('#umpireNameChange').val();
+				Email = $('#umpireEmailChange').val();
+			}
             
-            if(Name != "")
+            regexName = /^[A-Za-z\s\-]{1,30}$/;
+            regexEmail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+            if(regexName.test(Name))
                 validName = true;
-            if(Email != "")
-                validEmail=true;
-            
+            if(regexEmail.test(Email))
+                validEmail=true;     
             if(validName == true && validEmail==true)
-            {
                 validForm = true;
-            }
                 
         }
       
@@ -25,7 +40,7 @@ $(document).ready(function()
         
        $("#addUmpireValidation").click(function() 
         	{
-                    formControl();
+                    formControl("add");
                     var isChecked = new Array();
 					for(var i=0;i<14;i++)
 					{
@@ -44,7 +59,12 @@ $(document).ready(function()
         		  success: function(data, textStatus, jqXHR) {
         			  $('#addUmpire').modal('hide');
         			  $("#addUmpire :input").val("");
+        			  $("#addUmpire :checkbox").removeAttr("checked"); 
         			  $('#umpireList').html(data);
+        			  $('#addUmpireName').removeClass('control-group error');
+        			  $('#help-inline-umpireName').html('');
+        			  $('#addUmpireEmail').removeClass('control-group error');
+        			  $('#help-inline-umpireEmail').html('');
         		  },
         		  error: function(jqXHR, textStatus, errorThrown) {
         			  alert("Error during form validation, try later !");
@@ -56,42 +76,83 @@ $(document).ready(function()
                         if(validName == false)
                         {
                             $('#addUmpireName').addClass('control-group error');
-                            $('#help-inline-umpireName').html('<p>You must give a name for the umpire!');
+                            $('#help-inline-umpireName').html('<p>You must give a valid name for the umpire!');
                         }
+                        else
+                        {
+							$('#addUmpireName').removeClass('control-group error');
+							$('#help-inline-umpireName').html('');
+						}
                         if(validEmail == false)
                         {
                             $('#addUmpireEmail').addClass('control-group error');
-                            $('#help-inline-umpireEmail').html('<p>You must give an email address for the umpire!');
-                        }             
+                            $('#help-inline-umpireEmail').html('<p>You must give a valid email address for the umpire!');
+                        }      
+                        else
+                        {
+							$('#addUmpireEmail').removeClass('control-group error');
+							$('#help-inline-umpireEmail').html('');
+						}      
                     }
         		
         	});
         
         $("#editUmpireValidation").click(function() 
         	{
+				formControl("edit");
 				var isChecked = new Array();
 				for(var i=0;i<14;i++)
 				{
 					isChecked[i] = $("input[id=slott"+i+"]").is(":checked") ? 1:0; 
 				}
-        		jQuery.ajax({
-        		  type: 'GET',
-        		  url: 'ajax/editUmpire.php',
-        		  data: {
-        		    umpireName: $('#umpireNameChange').val(),
-        		    umpireEmail: $('#umpireEmailChange').val(),
-					id: $(this).attr('umpireID'),
-        		    checklist: isChecked
-        		  }, 
-        		  success: function(data, textStatus, jqXHR) {
-        			  $("#editUmpire").modal('hide');
-        			  $("#editUmpire :input").val("");
-        			  $('#umpireList').html(data);
-        		  },
-        		  error: function(jqXHR, textStatus, errorThrown) {
-        			  alert("Error during form validation, try later !");
-        		  }
-        		});
+				if(validForm == true)
+                {
+	        		jQuery.ajax({
+	        		  type: 'GET',
+	        		  url: 'ajax/editUmpire.php',
+	        		  data: {
+	        		    umpireName: $('#umpireNameChange').val(),
+	        		    umpireEmail: $('#umpireEmailChange').val(),
+						id: $(this).attr('umpireID'),
+	        		    checklist: isChecked
+	        		  }, 
+	        		  success: function(data, textStatus, jqXHR) {
+	        			  $("#editUmpire").modal('hide');
+	        			  $("#editUmpire :input").val("");
+	        			  $('#umpireList').html(data);
+	        			  $('#editUmpireName').removeClass('control-group error');
+	        			  $('#help-inline-umpireNameChange').html('');
+	        			  $('#editUmpireEmail').removeClass('control-group error');
+	        			  $('#help-inline-umpireEmailChange').html('');
+	        		  },
+	        		  error: function(jqXHR, textStatus, errorThrown) {
+	        			  alert("Error during form validation, try later !");
+	        		  }
+	        		 });
+				  }
+        		  else
+                    {
+                        if(validName == false)
+                        {
+                            $('#editUmpireName').addClass('control-group error');
+                            $('#help-inline-umpireNameChange').html('<p>You must give a valid name for the umpire!');
+                        }
+                        else
+                        {
+							$('#editUmpireName').removeClass('control-group error');
+							$('#help-inline-umpireNameChange').html('');
+						}
+                        if(validEmail == false)
+                        {
+                            $('#editUmpireEmail').addClass('control-group error');
+                            $('#help-inline-umpireEmailChange').html('<p>You must give a valid email address for the umpire!');
+                        }
+                        else
+                        {
+							$('#editUmpireEmail').removeClass('control-group error');
+							$('#help-inline-umpireEmailChange').html(''); 
+						}           
+                    }
         	});
         
         $(".btn-danger").live("click", function()
@@ -101,6 +162,14 @@ $(document).ready(function()
             $("#buttonDeleteUmpire").attr('umpireID',id);
                         
         });
+        
+        $("#editClose").click(function()
+        {
+			$('#editUmpireName').removeClass('control-group error');
+			$('#help-inline-umpireNameChange').html('');
+			$('#editUmpireEmail').removeClass('control-group error');
+			$('#help-inline-umpireEmailChange').html('');
+		});
         
         $("#buttonDeleteUmpire").click(function(){
             jQuery.ajax({
@@ -119,10 +188,17 @@ $(document).ready(function()
               });
         });
         
+		$('.checkall').click(function () {
+			//$("#addUmpire :checkbox").attr("checked",this.checked); 
+			$(this).parents('fieldset:eq(0)').find(':checkbox').attr('checked', this.checked);
+		});
+		
+        
         $(".btn-warning").live("click", function()
             	{
         			$('#editUmpire #umpireNameChange').val($(this).parent().parent().children('.umpireName').text());
         			$('#editUmpire #umpireEmailChange').val($(this).parent().parent().children('.umpireEmail').text());
+        			$('#editUmpire .checkall').removeAttr("checked");
         			for(var i=0;i<14;i++)
         			{
 						if($(this).parent().parent().children('.slot'+i).text() ==1)$('#editUmpire #slott'+i).attr('checked',true);
@@ -130,12 +206,5 @@ $(document).ready(function()
 					}
         			$('#editUmpire #editUmpireValidation').attr('umpireID',$(this).attr('id'));
             	});
-                
-       $('#addUmpire').on('hidden', function () {
-            $('#addUmpireName').removeClass('control-group error');
-            $('#help-inline-umpireName').html('');
-            $('#addUmpireEmail').removeClass('control-group error');
-            $('#help-inline-umpireEmail').html('');
-       });
         
 });
