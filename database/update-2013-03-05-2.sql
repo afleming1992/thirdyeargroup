76da5ec7b8eb6eab0f4a24986b348bca3584eff8
Yohann HAUTION <yohann.haution@gmail.com>
2013-03-05 15:55:19 +0000

    database update

---
+++ database/update-2013-03-05-2.sql
@@ -0,0 +1,5 @@
+ALTER TABLE hurdles_competitors ADD hurdlerLastName VARCHAR(100);
+ALTER TABLE hurdles_competitors ADD streetName VARCHAR(100);
+ALTER TABLE hurdles_competitors ADD houseNumber VARCHAR(100);
+ALTER TABLE hurdles_competitors ADD city VARCHAR(100);
+ALTER TABLE hurdles_competitors ADD postcode VARCHAR(100);
